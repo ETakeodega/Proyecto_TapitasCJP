f6dd0cc77981d9c2e97f1076f720e3061a363dd6
ETakeodega <enzo.suzuki@cjp.edu.py>
2024-06-05 10:54:52 -0400

    Correctiondeconteo
    Corregimos el contador, ya que este al ejecutar el sensor de proximidad, tiraba muy rapido los numeros, al hacer esta correccion hicimos que el conteo actual y el conteo anterior se tengan como diferentes y esto hizo que al contar imprima de uno en uno todo.

--- sketch_jun5a/sketch_jun5a.ino
+++ sketch_jun5a/sketch_jun5a.ino
@@ -1,7 +1,9 @@
 int sensor_pin = 4;
 int sensor_prox = 0;
 int contador_tapas = 0;
-
+int contador_r = 0;
+int contador_g = 0;
+int contador_r = 0;
 void setup() {
   // put your setup code here, to run once:
 pinMode (sensor_pin,INPUT);
@@ -14,10 +16,19 @@ sensor_prox = digitalRead (sensor_pin);
 //Serial.println (digitalRead (sensor_pin));
 
 if (sensor_prox){
-  Serial.println('R');
-  contador_tapas ++;
+  digitalRead (sensor_pin);
+  if (sensor_prox != digitalRead (sensor_pin)){
+    Serial.println('R');
+    contador_tapas ++;
 Serial.println(contador_tapas);
+  }
+  
+
+
+
+}
+
 }
 
 
-}
+
